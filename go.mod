ab0bea55be7e64178e0b4f781f15770aaf33c785
Victor Michel <victor.michel@elastic.co>
2023-03-29 19:05:49 -0700

    Make sure the zstd reader is closed

--- go.mod
+++ go.mod
@@ -3,8 +3,7 @@ module github.com/noborus/uncompressed
 go 1.13
 
 require (
-	github.com/frankban/quicktest v1.7.2 // indirect
-	github.com/klauspost/compress v1.10.1
-	github.com/pierrec/lz4 v2.4.1+incompatible
-	github.com/ulikunitz/xz v0.5.7
+	github.com/klauspost/compress v1.16.3
+	github.com/pierrec/lz4 v2.6.1+incompatible
+	github.com/ulikunitz/xz v0.5.11
 )
--- reader.go
+++ reader.go
@@ -8,7 +8,6 @@ import (
 	"bytes"
 	"compress/bzip2"
 	"io"
-	"io/ioutil"
 
 	"github.com/klauspost/compress/gzip"
 	"github.com/klauspost/compress/zstd"
@@ -23,10 +22,10 @@ func NewReader(reader io.Reader) io.ReadCloser {
 	n, err := io.ReadAtLeast(reader, buf[:], len(buf))
 	if err != nil {
 		if err == io.EOF || err == io.ErrUnexpectedEOF {
-			return ioutil.NopCloser(bytes.NewReader(buf[:n]))
+			return io.NopCloser(bytes.NewReader(buf[:n]))
 		}
 		// Should errors not happen?
-		return ioutil.NopCloser(bytes.NewReader(nil))
+		return io.NopCloser(bytes.NewReader(nil))
 	}
 
 	rd := io.MultiReader(bytes.NewReader(buf[:n]), reader)
@@ -35,20 +34,20 @@ func NewReader(reader io.Reader) io.ReadCloser {
 	case bytes.Equal(buf[:3], []byte{0x1f, 0x8b, 0x8}):
 		r, err = gzip.NewReader(rd)
 	case bytes.Equal(buf[:3], []byte{0x42, 0x5A, 0x68}):
-		r = ioutil.NopCloser(bzip2.NewReader(rd))
+		r = io.NopCloser(bzip2.NewReader(rd))
 	case bytes.Equal(buf[:4], []byte{0x28, 0xb5, 0x2f, 0xfd}):
 		var zr *zstd.Decoder
 		zr, err = zstd.NewReader(rd)
-		r = ioutil.NopCloser(zr)
+		r = zr.IOReadCloser()
 	case bytes.Equal(buf[:4], []byte{0x04, 0x22, 0x4d, 0x18}):
-		r = ioutil.NopCloser(lz4.NewReader(rd))
+		r = io.NopCloser(lz4.NewReader(rd))
 	case bytes.Equal(buf[:7], []byte{0xfd, 0x37, 0x7a, 0x58, 0x5a, 0x0, 0x0}):
 		var zr *xz.Reader
 		zr, err = xz.NewReader(rd)
-		r = ioutil.NopCloser(zr)
+		r = io.NopCloser(zr)
 	}
 	if err != nil || r == nil {
-		r = ioutil.NopCloser(rd)
+		r = io.NopCloser(rd)
 	}
 	return r
 }
